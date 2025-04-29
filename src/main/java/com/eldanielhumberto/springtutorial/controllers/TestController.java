0ede82c481498949aa5f0891480d522cec31f46c
Humberto Daniel <gamekik92@gmail.com>
2025-04-29 12:32:53 -0600

    feat: using ModelAttribute

--- src/main/java/com/eldanielhumberto/springtutorial/controllers/TestController.java
+++ src/main/java/com/eldanielhumberto/springtutorial/controllers/TestController.java
@@ -1,8 +1,12 @@
 package com.eldanielhumberto.springtutorial.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.eldanielhumberto.springtutorial.models.Student;
 import com.eldanielhumberto.springtutorial.models.dto.StudentDTO;
@@ -18,4 +22,12 @@ public class TestController {
 
         return "hello-world";
     }
+
+    @ModelAttribute("Students")
+    public List<Student> studentsList() {
+        return Arrays.asList(
+                new Student("Humberto", "Ramirez", 2),
+                new Student("Jose Santiago", "Altamirano", 2));
+    }
+
 }
