c82c7013a35ee24c94b4810e7b34c4c6c9fd33f1
Humberto Daniel <gamekik92@gmail.com>
2025-05-01 16:48:13 -0600

    feat: using POST requests

--- src/main/java/com/eldanielhumberto/springtutorial/controllers/TestRestController.java
+++ src/main/java/com/eldanielhumberto/springtutorial/controllers/TestRestController.java
@@ -1,18 +1,16 @@
 package com.eldanielhumberto.springtutorial.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// import com.eldanielhumberto.springtutorial.models.Student;
+import com.eldanielhumberto.springtutorial.models.Student;
 import com.eldanielhumberto.springtutorial.models.dto.StudentDTO;
 
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/test-rest")
@@ -24,18 +22,14 @@ public class TestRestController {
         return new StudentDTO(fullname, schoolYear);
     }
 
-    @GetMapping("/get/{fullname}")
+    @GetMapping("/{fullname}")
     public StudentDTO getPathVariableTest(@PathVariable String fullname,
             @RequestParam(required = false) Integer schoolYear) {
         return new StudentDTO(fullname, schoolYear);
     }
 
-    @RequestMapping(path = "/post", method = RequestMethod.POST)
-    public Map<String, Object> postTest() {
-        Map<String, Object> response = new HashMap<>();
-        response.put("message", "Hello post");
-
-        return response;
+    @PostMapping("/post")
+    public Student postTest(@RequestBody Student entity) {
+        return entity;
     }
-
 }
