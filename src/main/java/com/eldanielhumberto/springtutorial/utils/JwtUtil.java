47ca08f315aeb5003740d8caed7ac96b93b8ce1f
Humberto Daniel <gamekik92@gmail.com>
2025-05-12 23:46:09 -0600

    feat: preparing jwt and refactoring

---
+++ src/main/java/com/eldanielhumberto/springtutorial/utils/JwtUtil.java
@@ -0,0 +1,42 @@
+package com.eldanielhumberto.springtutorial.utils;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtUtil {
+    private final String SECRET_KEY = "mi_clave_super_segura_1234567890!!!";
+    private final Key key = new SecretKeySpec(SECRET_KEY.getBytes(), SignatureAlgorithm.HS256.getJcaName());
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token, String username) {
+        return extractUsername(token).equals(username) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+}
